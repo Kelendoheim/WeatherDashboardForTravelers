5fd044459553f8891467ecf96d838793efd6828a
TDWalsh <thomas.walsh736@gmail.com>
2020-09-15 21:14:19 -0400

    added a for loop that populates the page with buttons for each searched city

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 var submitEl = $(".submitBtn");
 var cityInputEl = $(".cityInput");
 var cityArray = [];
+var searchedCitiesEl = $("#searchedCities");
 
 
 $(document).on("click", ".submitBtn", function (event) {
     event.preventDefault();
+    searchedCitiesEl.empty();
     cityArray.unshift($(event.target).siblings().val());
     console.log(cityArray);
     localStorage.setItem(
         "cities",
         cityArray
       );
+      for(i = 0; i < cityArray.length; i++){
+        var breakEl = $("<br>");
+        var cityButtons = $("<div>");
+        cityButtons.attr("class", "row btn btn-primary col-sm-12");
+        cityButtons.attr("id", cityArray[i])
+        cityButtons.text(cityArray[i])
+        searchedCitiesEl.append(breakEl)
+        searchedCitiesEl.append(cityButtons)
+      }
   });
+
+  
